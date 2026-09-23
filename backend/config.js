c9a147752a87e9c4c01d9e7578a91e6b17b1c2d2
marmara <marmara@marmaras-MacBook-Air.local>
2026-09-23 23:46:50 +0300

    Render deploy yapilandirmasi ekle, CORS'u ortam degiskeniyle yapilandir

--- backend/config.js
+++ backend/config.js
@@ -5,5 +5,7 @@ module.exports = {
   bcryptRounds: 10,
   dbPath: './db/etiket-avcisi.db',
   maxErrorHistory: 100,
-  corsOrigins: ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500']
+  corsOrigins: process.env.CORS_ORIGINS
+    ? process.env.CORS_ORIGINS.split(',')
+    : ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500']
 };
